96beb4b9bfc046305bc209d217627396ba623171
Samira Rabbanian <samira.rabanian@gmail.com>
2014-06-15 19:29:20 +0100

    improving the test go file to be a basic HTTP server

--- test.go
+++ test.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"time"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("\nThis docker containter does actually run go!!\n")
+	//create listener
+	listener, err := net.Listen("tcp", "0.0.0.0:8080")
+	if err != nil {
+		fmt.Println("Error Listening", err.Error())
+		return // terminate program
+	}
+
+	// listen and accept connections from clients:
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting", err.Error())
+			return // terminate program
+		}
+		go doServerStuff(conn)
+	}
+}
+
+func doServerStuff(conn net.Conn) {
+	body := "<html><head></head><body><div>This is a simple go server running in a docker containers</div></body></html>\n"
+	conn.Write([]byte("HTTP/1.1 200 Ok\n" +
+			"Content-Length: " + strconv.Itoa(len(body)) + "\n" +
+			"Set-Cookie: JSESSIONID=F9ADC2A30C8255A49061992F48C8AAAB; Path=/; Secure; HttpOnly\n" +
+			"\r\n" +
+			body))
+
+	time.Sleep(10 * time.Millisecond)
+	conn.Close()
 }
