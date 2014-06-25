dcbd18bb838003409ef855b1ef143d592363f00a
Samira Rabbanian <samira.rabanian@gmail.com>
2014-06-25 09:19:32 +0100

    added a new test server

--- test.go
+++ test.go
@@ -2,38 +2,33 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"time"
 	"strconv"
+	"net/http"
 )
 
 func main() {
-	//create listener
-	listener, err := net.Listen("tcp", "0.0.0.0:8080")
-	if err != nil {
-		fmt.Println("Error Listening", err.Error())
-		return // terminate program
-	}
-
-	// listen and accept connections from clients:
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting", err.Error())
-			return // terminate program
-		}
-		go doServerStuff(conn)
-	}
+	Server(8080)
 }
 
-func doServerStuff(conn net.Conn) {
-	body := "<html><head></head><body><div>This is a simple go server running in a docker containers</div></body></html>\n"
-	conn.Write([]byte("HTTP/1.1 200 Ok\n" +
-			"Content-Length: " + strconv.Itoa(len(body)) + "\n" +
-			"Set-Cookie: JSESSIONID=F9ADC2A30C8255A49061992F48C8AAAB; Path=/; Secure; HttpOnly\n" +
-			"\r\n" +
-			body))
+func Server(port int) {
+	fmt.Println("Starting server " + strconv.Itoa(port) + " ....")
+	http.ListenAndServe(":"+strconv.Itoa(port), &handle{port})
+}
 
+type handle struct {
+	Port int
+}
+
+func (h *handle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(10 * time.Millisecond)
-	conn.Close()
+	fmt.Fprintf(w, "ONE, ")
+	time.Sleep(10 * time.Millisecond)
+	fmt.Fprintf(w, "TWO, ")
+	time.Sleep(10 * time.Millisecond)
+	fmt.Fprintf(w, "THREE, ")
+	time.Sleep(10 * time.Millisecond)
+	fmt.Fprintf(w, "FOUR, ")
+	time.Sleep(10 * time.Millisecond)
+	fmt.Fprintf(w, "FIVE from %d\n", h.Port)
 }
