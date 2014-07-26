60a462e164d8f7f29726131304bcb6280b628bcb
Samira Rabbanian <samira.rabanian@gmail.com>
2014-07-26 17:27:12 +0100

    improved netty code to bring it inline with the example go http server

--- example_servers/netty_server/src/TestServer.java
+++ example_servers/netty_server/src/TestServer.java
@@ -4,17 +4,13 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.DefaultFullHttpResponse;
-import io.netty.handler.codec.http.FullHttpResponse;
-import io.netty.handler.codec.http.HttpRequest;
-import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.*;
 import io.netty.handler.logging.LoggingHandler;
 
 import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
 
-import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
-import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
-import static io.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
+import static io.netty.handler.codec.http.HttpHeaders.Names.*;
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
@@ -49,10 +45,10 @@ public class TestServer {
 
     private static class TestServerHandler extends SimpleChannelInboundHandler<Object> {
         int port;
-        public TestServerHandler(int _port){
+
+        public TestServerHandler(int _port) {
             port = _port;
         }
-
 
         @Override
         public void channelReadComplete(ChannelHandlerContext ctx) {
@@ -63,19 +59,76 @@ public class TestServer {
         public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
             if (msg instanceof HttpRequest) {
                 HttpRequest req = (HttpRequest) msg;
+                if (req.getUri().equals("/chunked")) {
+                    HttpResponse response = new DefaultHttpResponse(HTTP_1_1, OK);
+                    response.headers().set(CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+                    response.headers().set(PRAGMA, "no-cache");
+                    response.headers().set(EXPIRES, "0");
+                    response.headers().set(HttpHeaders.Names.TRANSFER_ENCODING, HttpHeaders.Values.CHUNKED);
+                    response.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+                    ctx.write(response);
 
-                FullHttpResponse response;
-                if (req.getUri().equals("/unknown")) {
-                    response = new DefaultFullHttpResponse(HTTP_1_1, NOT_FOUND);
-                } else {
-                    String serverResponse = "Response from netty server on port: " + port+ "\n";
+                    ctx.write(new DefaultHttpContent(Unpooled.wrappedBuffer(String.format("NS Port: %d\n", port).getBytes(Charset.forName("UTF-8")))));
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    ctx.write(new DefaultHttpContent(Unpooled.wrappedBuffer("50 ms, ".getBytes(Charset.forName("UTF-8")))));
+                    ctx.flush();
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    ctx.write(new DefaultHttpContent(Unpooled.wrappedBuffer("100 ms, ".getBytes(Charset.forName("UTF-8")))));
+                    ctx.flush();
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    ctx.write(new DefaultHttpContent(Unpooled.wrappedBuffer("150 ms, ".getBytes(Charset.forName("UTF-8")))));
+                    ctx.flush();
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    ctx.write(new DefaultHttpContent(Unpooled.wrappedBuffer("200 ms, ".getBytes(Charset.forName("UTF-8")))));
+                    ctx.flush();
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    ctx.write(new DefaultHttpContent(Unpooled.wrappedBuffer("250 ms\n".getBytes(Charset.forName("UTF-8")))));
+                    ctx.flush();
+
+                    // close
+                    ctx.write(new DefaultHttpContent(Unpooled.wrappedBuffer("\r\n".getBytes(Charset.forName("UTF-8")))));
+                    ctx.write(new DefaultHttpContent(Unpooled.wrappedBuffer("".getBytes(Charset.forName("UTF-8")))));
+                    ctx.write(new DefaultHttpContent(Unpooled.wrappedBuffer(new byte[]{})));
+                    ctx.write(new DefaultHttpContent(Unpooled.EMPTY_BUFFER));
+                    ctx.flush();
+                } else if (req.getUri().equals("/close")) {
+                    FullHttpResponse response;
+                    String serverResponse = String.format("NS Port: %d\n", port);
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    serverResponse += "50 ms, ";
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    serverResponse += "100 ms, ";
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    serverResponse += "150 ms, ";
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    serverResponse += "200 ms, ";
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    serverResponse += "250 ms\n";
                     response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(serverResponse.getBytes(Charset.forName("UTF-8"))));
-                    response.headers().set(CONTENT_TYPE, "text/plain");
                     response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+                    response.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    FullHttpResponse response;
+                    String serverResponse = String.format("NS Port: %d\n", port);
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    serverResponse += "50 ms, ";
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    serverResponse += "100 ms, ";
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    serverResponse += "150 ms, ";
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    serverResponse += "200 ms, ";
+                    TimeUnit.MILLISECONDS.sleep(50);
+                    serverResponse += "250 ms\n";
+                    response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(serverResponse.getBytes(Charset.forName("UTF-8"))));
+                    response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+                    response.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+                    ctx.write(response);
                 }
-                ctx.write(response).addListener(ChannelFutureListener.CLOSE);
             }
         }
+
         @Override
         public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
             cause.printStackTrace();
